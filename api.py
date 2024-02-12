954844781095df168a153ec78f680b56f5e98a24
DimaGurevich <dgurevich@metanetworks.com>
2024-02-12 16:55:06 +0200

    api.py : class for api https://gorest.co.in/

--- api.py
+++ api.py
@@ -1,19 +1,10 @@
 import base64
 
+from Consts import API_BASE_URL, TOKEN
+
 
 class api:
-    def __init__(self, userName, password):
+    def __init__(self, endpoint):
         # for docker need to use : host.docker.internal , for your local machine : localhost.
-        self.apiUrl = 'http://host.docker.internal:8000/players?page={0}'
-        self.setAuthorization(userName, password)
-
-    '''
-    should work only admin:admin pair.
-    '''
-    def setAuthorization(self, userName, password):
-        self.userName = userName
-        self.password = password
-        authPair = ('{0}:{1}'.format(userName, password))
-        message_bytes = authPair.encode('ascii')
-        base64_bytes = base64.b64encode(message_bytes)
-        self.authorization = "Basic {0}".format(str(base64_bytes.decode('ascii')))
+        self.api_endpoint = f'{API_BASE_URL}/{endpoint}'
+        self.authorization = f'Bearer {TOKEN}'
